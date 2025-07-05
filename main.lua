d9dda8d85e694185cf7854fc34bb10f50302d291
Lasha Goginava <lashagoginava31@gmail.com>
2025-07-05 16:41:18 +0400

    add GameState tracking

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 local Dog = require "dog"
 local Light = require "light"
+local Menu = require "menu"
+
+GameState = {
+  MENU = "menu",
+  GAME = "game",
+  LIGHT_LEVEL = "light_level"
+}
+
+CurrentState = GameState.MENU
 
 function love.load()
   DEBUG_MODE = true
@@ -17,31 +26,37 @@ function love.load()
 end
 
 function love.update(dt)
-  Dog:update(dt)
-  Light:update(dt)
+  if CurrentState == GameState.MENU then
+    Menu:update(dt)
+  elseif CurrentState == GameState.GAME then
+    Dog:update(dt)
+    Light:update(dt)
+  end
 end
 
 function love.draw()
-  love.graphics.draw(backgroundImage, 0, 0, 0, 0.8, 0.8) --0.8x scale temp
-  Dog:draw()
-  Light:draw()
+  if CurrentState == GameState.MENU then
+    Menu:draw()
+  elseif CurrentState == GameState.GAME then
+    love.graphics.draw(backgroundImage, 0, 0, 0, 0.8, 0.8) --0.8x scale temp
+    Dog:draw()
+    Light:draw()
+  end
 
-  if(DEBUG_MODE) then
+  if (DEBUG_MODE) then
     local mx, my = love.mouse.getPosition()
     love.graphics.print("X:" .. mx .. " Y:" .. my, 10, 10)
   end
 end
 
 function love.keypressed(key)
-  HandleLightToggle(key)
+  if CurrentState == GameState.LIGHT_LEVEL or CurrentState == GameState.GAME then --Temp. make only light level in future
+    Light:keypressed(key)
+  end
 end
 
-function HandleLightToggle(key)
-  if key == "e" then
-    if Light.enabled then
-      Light.disable()
-    else
-      Light.enable()
-    end
+function love.mousepressed(x, y, button)
+  if CurrentState == GameState.MENU then
+    Menu:mousepressed(x, y, button)
   end
 end
