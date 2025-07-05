2d521ab65f9c9f173bd49822264067fb829a3d61
Lasha Goginava <lashagoginava31@gmail.com>
2025-07-05 13:13:48 +0400

    main

---
+++ main.lua
@@ -0,0 +1,17 @@
+function love.load()
+  WIDTH = 1620
+  HEIGHT = 980
+  love.window.setMode(WIDTH, HEIGHT)
+  love.window.setTitle("Amirani")
+
+  -- Set the random seed
+  math.randomseed(os.time())
+end
+
+function love.update(dt)
+    
+end
+  
+function love.draw()
+
+end
